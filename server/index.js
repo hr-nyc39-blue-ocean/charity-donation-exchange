46a08861ae192a9db85bca1ce8d8fb2ec936d85f
John Hwang <juhanedang@gmail.com>
2021-12-08 20:06:16 -0500

    initialization

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 const axios = require('axios');
 const morgan = require('morgan');
 // const db = require('./db/index.js');
