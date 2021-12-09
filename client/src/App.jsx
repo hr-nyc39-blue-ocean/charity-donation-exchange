0d471386bfe34017e4c89e21a22cb01bdfa2034b
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-09 16:58:19 -0500

    converted to functional component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 
-import axios from 'axios';
-import styled from 'styled-components';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render () {
-    return (
-    <div>
-      Hello, this is APP
-    </div>)
-  }
-}
-
+const App = () => {
+  return <div>Hello, this is APP</div>;
+};
 
 export default App;
-
