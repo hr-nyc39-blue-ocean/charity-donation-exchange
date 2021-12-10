2ba81fc4d3b5e6323f2a6ad42fa62007b0e601ea
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-09 20:23:59 -0500

    added buttons and logic for each onclick

---
+++ client/src/components/LoggedInHeader.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "../shared/Button.jsx";
+
+const LoggedInHeader = ({ setIsLoggedIn, setDashboardClicked }) => {
+  const handleLogoutOnClick = () => {
+    setIsLoggedIn(false);
+  };
+  const handleDashboardOnClick = () => {
+    setDashboardClicked(true);
+  };
+  return (
+    <div style={{ height: "25vh", width: "100vw", border: "1px solid black" }}>
+      <div>
+        <div>LOGO</div>
+        <div>
+          <Button handleOnClick={handleLogoutOnClick} text="Logout" />
+          <Button handleOnClick={handleDashboardOnClick} text="Dashboard" />
+        </div>
+      </div>
+      WELCOME, NAME!
+    </div>
+  );
+};
+
+export default LoggedInHeader;
