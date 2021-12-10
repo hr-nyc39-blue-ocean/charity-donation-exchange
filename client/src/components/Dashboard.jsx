efc33196380cd16a4ac671ae341e4811e134ed1a
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-09 20:51:16 -0500

    renders Dashboard Header and DonationList component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import DonationList from "../shared/DonationList.jsx";
 import DashboardHeader from "./DashboardHeader.jsx";
 
-const Dashboard = ({ setDashboardClicked, setIsLoggedIn }) => {
+const Dashboard = ({
+  setDashboardClicked,
+  setIsLoggedIn,
+  dashboardClicked,
+}) => {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <DashboardHeader
         setDashboardClicked={setDashboardClicked}
         setIsLoggedIn={setIsLoggedIn}
       />
-      <DonationList width={100} height={70} />
+      <DonationList
+        width={100}
+        height={70}
+        dashboardClicked={dashboardClicked}
+      />
     </div>
   );
 };
