cfcedcc9138ede1692883099a14636be47591a90
Richard Hyesung O <orichh@gmail.com>
2021-12-10 10:41:37 -0500

    Initialize branch and add basic endpoints

--- server/index.js
+++ server/index.js
@@ -1,23 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const axios = require('axios');
-const morgan = require('morgan');
+const axios = require("axios");
+const morgan = require("morgan");
 // const db = require('./db/index.js');
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + "/../client/dist"));
 
+// GET requests
+app.get("/getAllDonations", (req, res) => {
+  res.status(200).send("ok");
+});
+app.get("/dashboardDonations", (req, res) => {
+  res.status(200).send("ok");
+});
 
+// POST requests
+app.post("newUserRegistration", (req, res) => {
+  res.status(200).send("ok");
+});
+app.post("addNewDonation", (req, res) => {
+  res.status(200).send("ok");
+});
 
+// PUT requests
+app.put("claimDonation", (req, res) => {
+  res.status(200).send("ok");
+});
 
+// DELETE requests
+app.delete("deleteDonationListing", (req, res) => {
+  res.status(200).send("ok");
+});
 
+/*
+TODO: how to handle user login and logout?
+user login
+user logout
 
-
-
+*/
 
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${PORT}!`);
