f3a103ba01a65011da9afdf743bbd4772e528762
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-11 17:16:33 -0500

    added styles

--- client/src/shared/Donation.jsx
+++ client/src/shared/Donation.jsx
@@ -1,33 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button.jsx";
+import Modal from "../shared/Modal.jsx";
 
 const Donation = ({
-  dashboardClicked,
+  listingId = 1,
+  showDashboard,
   name = "Jeans",
   category = "Clothing",
-  amount = "$2.00",
+  quantity = "1",
   location = "Queens",
   claimStatus = null,
 }) => {
+  const [showClaimModal, setShowClaimModal] = useState(false);
+
+  const toggleClaimModal = (e) => {
+    setShowClaimModal(!showClaimModal);
+  };
+
+  const handleCancelListingOnClick = () => {
+    // dont delete listing from db, change status to inactive
+    // inactive listings do not show on listings page on homepage but show on dashboard
+  };
+
+  const MarkCompleteOnClick = () => {
+    // mark status to complete in db
+  };
+
   return (
-    <div>
-      {dashboardClicked ? (
-        <div>
-          <div>Item name: {name}</div>
-          <div>Category: {category}</div>
-          <div>Location: {location}</div>
-          <div>Claim status: {claimStatus}</div>
-          <Button text="Cancel Listing" />
-          <Button text="Mark As Complete" />
+    <div className="donation-container">
+      {showDashboard ? (
+        <div className="donation-card-layout">
+          <div>
+            <div>Id: {listingId}</div>
+            <div>Item name: {name}</div>
+            <div>Category: {category}</div>
+            <div>Quantity: {quantity}</div>
+            <div>Location: {location}</div>
+            <div>Claim status: {claimStatus}</div>
+          </div>
+          <div className="donation-card-buttons">
+            <div className="donation-button-styles">
+              <Button text="Cancel Listing" />
+            </div>
+            <div className="donation-button-styles">
+              <Button text="Mark As Complete" />
+            </div>
+          </div>
         </div>
       ) : (
-        <div>
-          <div>Item name: {name}</div>
-          <div>Category: {category}</div>
-          <div>Amount: {amount}</div>
-          <div>Location: {location}</div>
-          <Button text="Claim" />
+        <div className="donation-card-layout">
+          <div>
+            <div>Listing Id: {listingId}</div>
+            <div>Item name: {name}</div>
+            <div>Category: {category}</div>
+            <div>Quantity: {quantity}</div>
+            <div>Location: {location}</div>
+          </div>
+          <div className="donation-card-buttons">
+            <Button handleOnClick={toggleClaimModal} text="Claim" />
+          </div>
         </div>
+      )}
+      {showClaimModal && (
+        <Modal
+          listingId={listingId}
+          claimModal={showClaimModal}
+          toggleModal={toggleClaimModal}
+        />
       )}
     </div>
   );
