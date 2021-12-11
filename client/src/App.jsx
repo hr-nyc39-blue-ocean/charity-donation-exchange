290d0fa10a9cd8124d078de06cfa1847102fe97b
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-11 17:13:24 -0500

    added styles and state to pass down as props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import Home from "./components/Home.jsx";
 import Dashboard from "./components/Dashboard.jsx";
-import "./styles/shared/Logo.scss";
-import "./styles/Global.scss";
 
 const App = () => {
   const [showDashboard, setShowDashboard] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
 
   return (
-    <>
+    <div className="global">
       {showDashboard ? (
         <Dashboard
+          userId={userId}
           showDashboard={showDashboard}
           setShowDashboard={setShowDashboard}
           setIsLoggedIn={setIsLoggedIn}
         />
       ) : (
         <Home
+          setUserId={setUserId}
           showDashboard={showDashboard}
           setShowDashboard={setShowDashboard}
           isLoggedIn={isLoggedIn}
           setIsLoggedIn={setIsLoggedIn}
         />
       )}
-    </>
+    </div>
   );
 };
 
