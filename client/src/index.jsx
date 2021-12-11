d82e16b5bba366c7d17d83bd77a33faea1f21bb0
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-11 17:13:38 -0500

    imported sass files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import "./styles/Global.scss";
+import "./styles/Home.scss";
+import "./styles/Headers.scss";
+import "./styles/Dashboard.scss";
+import "./styles/NavBar.scss";
+import "./styles/DonationList.scss";
+import "./styles/Donation.scss";
 
 ReactDOM.render(<App />, document.getElementById("app"));
