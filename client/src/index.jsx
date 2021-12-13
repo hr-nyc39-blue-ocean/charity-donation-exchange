9100ef8c03a9be9afd375038ab0c222c4bbe0658
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-13 12:34:53 -0500

    imported scss files to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./styles/Global.scss";
-import "./styles/Home.scss";
 import "./styles/Headers.scss";
-import "./styles/Dashboard.scss";
 import "./styles/NavBar.scss";
 import "./styles/DonationList.scss";
 import "./styles/Donation.scss";
+import "./styles/Button.scss";
+import "./styles/SortBy.scss";
 
 ReactDOM.render(<App />, document.getElementById("app"));
