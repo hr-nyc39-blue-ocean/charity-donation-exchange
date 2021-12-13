c45dd8492a82b0c0f84a7eea8eb900e4b85586c4
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-13 12:40:53 -0500

    added Donation styles

--- client/src/shared/Donation.jsx
+++ client/src/shared/Donation.jsx
@@ -3,13 +3,15 @@ import Button from "./Button.jsx";
 import Modal from "../shared/Modal.jsx";
 
 const Donation = ({
+  fetch,
   listingId = 1,
+  date = "12/13/2021",
   showDashboard,
   name = "Jeans",
   category = "Clothing",
   quantity = "1",
   location = "Queens",
-  claimStatus = null,
+  claimStatus = "Pending",
 }) => {
   const [showClaimModal, setShowClaimModal] = useState(false);
 
@@ -20,47 +22,82 @@ const Donation = ({
   const handleCancelListingOnClick = () => {
     // dont delete listing from db, change status to inactive
     // inactive listings do not show on listings page on homepage but show on dashboard
+    // refetch donations to refresh
   };
 
-  const MarkCompleteOnClick = () => {
+  const handleMarkCompleteOnClick = () => {
     // mark status to complete in db
+    // refetch donations to refresh
   };
+
+  const dashboardButtons = [
+    {
+      text: "Cancel Listing",
+      handleOnClick: handleCancelListingOnClick,
+    },
+    {
+      text: "Mark As Complete",
+      handleOnClick: handleMarkCompleteOnClick,
+    },
+  ];
 
   return (
     <div className="donation-container">
-      {showDashboard ? (
-        <div className="donation-card-layout">
+      <div className="donation-card-layout">
+        <div>
           <div>
-            <div>Id: {listingId}</div>
-            <div>Item name: {name}</div>
-            <div>Category: {category}</div>
-            <div>Quantity: {quantity}</div>
-            <div>Location: {location}</div>
-            <div>Claim status: {claimStatus}</div>
+            <span className="dontation-card-title">Listing ID: </span>
+            {listingId}
           </div>
-          <div className="donation-card-buttons">
-            <div className="donation-button-styles">
-              <Button text="Cancel Listing" />
-            </div>
-            <div className="donation-button-styles">
-              <Button text="Mark As Complete" />
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="donation-card-layout">
           <div>
-            <div>Listing Id: {listingId}</div>
-            <div>Item name: {name}</div>
-            <div>Category: {category}</div>
-            <div>Quantity: {quantity}</div>
-            <div>Location: {location}</div>
+            <span className="dontation-card-title">Date: </span>
+            {date}
           </div>
-          <div className="donation-card-buttons">
-            <Button handleOnClick={toggleClaimModal} text="Claim" />
+          <div>
+            <span className="dontation-card-title">Item name: </span>
+            {name}
           </div>
+          <div>
+            <span className="dontation-card-title">Category: </span>
+            {category}
+          </div>
+          <div>
+            <span className="dontation-card-title">Quantity: </span>
+            {quantity}
+          </div>
+          <div>
+            <span className="dontation-card-title">Location: </span>
+            {location}
+          </div>
+          {showDashboard && (
+            <div>
+              <span className="dontation-card-title">Claim status: </span>
+              <span>{claimStatus}</span>
+            </div>
+          )}
         </div>
-      )}
+        {showDashboard ? (
+          <div className="donation-card-buttons">
+            {dashboardButtons.map((d) => {
+              return (
+                <Button
+                  className="donation-button-styles"
+                  text={d.text}
+                  handleOnClick={d.handleOnClick}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div className="donation-card-buttons">
+            <Button
+              handleOnClick={toggleClaimModal}
+              text="Claim"
+              className="donation-button-styles"
+            />
+          </div>
+        )}
+      </div>
       {showClaimModal && (
         <Modal
           listingId={listingId}
