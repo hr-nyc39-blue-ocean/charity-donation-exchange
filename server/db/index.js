0cbd6a726e1c83c8f83bf14afd4ae8546f8a435f
Richard Hyesung O <orichh@gmail.com>
2021-12-14 18:40:54 -0500

    Additional docker setup

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
 const db = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "blueocean2",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  // port: 3306,
 });
 
 db.getConnection((err) => {
