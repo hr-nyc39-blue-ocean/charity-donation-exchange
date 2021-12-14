81ffcfb04b022003f3a4a000bf88c731c0e100ce
John Hwang <juhanedang@gmail.com>
2021-12-14 17:13:04 -0500

    latest changes pulled

--- client/src/shared/Donation.jsx
+++ client/src/shared/Donation.jsx
@@ -115,7 +115,7 @@ const Donation = ({
           </div>
         </div>
         <div className="donation-claimed-details-container">
-          {distance && (
+          {distance >= 0 && (
             <div className="donation-distance-container">
               <span className="dontation-card-title">Distance: </span>
               <span>{`${distance} miles`}</span>
