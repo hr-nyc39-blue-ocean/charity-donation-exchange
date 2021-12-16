d07138ca82a2214f2d81424225ee1806cd520128
jaimienakayama <jaimie.nakayama@icloud.com>
2021-12-16 12:23:19 -0500

    ready for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "blueocean2",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "blueocean2",
+  host: process.env.HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
